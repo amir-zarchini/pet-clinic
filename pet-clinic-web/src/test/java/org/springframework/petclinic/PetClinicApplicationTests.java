c359aaf5e865d2c950d874337cf9c91747ebbb27
Amir_zr <amzarchini@gmail.com>
2021-08-03 14:08:27 +0430

    bug fix: update version pom and fix test class implement interface for services. closes #5

--- pet-clinic-web/src/test/java/org/springframework/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/org/springframework/petclinic/PetClinicApplicationTests.java
@@ -1,6 +1,6 @@
-package petclinic;
+package org.springframework.petclinic;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
